Fixed issue where transformations on dataset were happening twice. Once during model evaluation and second during output file creation. As a result of this double translation there was double scaling. Also fixed issue with predicted masks.

test.py
@@ -1,4 +1,3 @@
-import torch
 from torchvision.models.detection import maskrcnn_resnet50_fpn
 from torchvision.models.detection.rpn import AnchorGenerator, RPNHead
 from torch.utils.data import DataLoader
@@ -17,8 +16,8 @@ NORM_SPACING = 0.8  # Resize every image slice so that each pixel corresponds to
 MAX_SIZE = 512
 
 DIR_IN = 'Images_png'  # input directory
-GT_FN_TRAIN = 'DL_info_train.csv'  # Ground truth file for training data
-GT_FN_VAL = 'DL_info_val.csv'  # Ground truth file for validation data
+GT_FN_TRAIN = 'DL_info_train_sample.csv'  # Ground truth file for training data
+GT_FN_VAL = 'DL_info_val_sample.csv'  # Ground truth file for validation data
 GT_FN_TEST = 'DL_info_test_sample.csv'  # Ground truth file for test data
 GT_FN_DICT = {"train": GT_FN_TRAIN, "val": GT_FN_VAL, "test": GT_FN_TEST}
 
@@ -57,18 +56,25 @@ def main():
                             , T.ToTensor()])
         , 'test': T.Compose([T.ToOriginalHU(INTENSITY_OFFSET)
                             , T.IntensityWindowing(WINDOWING)
-                            # , T.SpacingResize(NORM_SPACING, MAX_SIZE)
+                            , T.SpacingResize(NORM_SPACING, MAX_SIZE)
                             , T.ToTensor()])
     }
     image_datasets = {x: DeepLesion(DIR_IN + os.sep + x, GT_FN_DICT[x], data_transforms[x]) for x in ['train', 'val'
                                                                                                       , 'test']}
+
+    image_datasets_dup = {x: DeepLesion(DIR_IN + os.sep + x, GT_FN_DICT[x], data_transforms[x]) for x in ['train', 'val'
+                                                                                                          , 'test']}
     dataloaders = {x: DataLoader(image_datasets[x], batch_size=3, shuffle=True, num_workers=0
                                  , collate_fn=BatchCollator) for x in ['train', 'val', 'test']}
+
+    dataloaders_dup = {x: DataLoader(image_datasets_dup[x], batch_size=3, shuffle=True, num_workers=0
+                                     , collate_fn=BatchCollator) for x in ['train', 'val', 'test']}
     output = test_model(model, dataloaders['test'])
-    predbox = output[0]['boxes'][4].numpy()
-    # predmask = output[0]['masks'][4].squeeze().numpy()
-    # print(output[0]['scores'][4].numpy())
-    for batch_id, (inputs, targets) in enumerate(dataloaders['test']):
+    predbox = output[0]['boxes'][0].numpy()
+    predmask = output[0]['masks'][0].squeeze().numpy()
+    predmask = np.where(predmask > 0.5, 1, 0)
+    print(output[0]['scores'][0].numpy())
+    for batch_id, (inputs, targets) in enumerate(dataloaders_dup['test']):
         i = 0
         for i, (image, target) in enumerate(zip(inputs, targets)):
             img_copy = image.squeeze().numpy()
@@ -76,17 +82,17 @@ def main():
             images = [im.astype(float) for im in images]
             img_copy = cv2.merge(images)
             for j, (bbox, pseudo_mask) in enumerate(zip(target["boxes"], target["masks"])):
-                bbox = target["boxes"][j].squeeze().numpy()
+                bbox = bbox.squeeze().numpy()
                 bbox = np.int16(bbox)
-                mask = target["masks"][j].squeeze().numpy()
+                mask = pseudo_mask.squeeze().numpy()
                 cv2.rectangle(img_copy, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 255, 0), 1)
                 cv2.rectangle(img_copy, (predbox[0], predbox[1]), (predbox[2], predbox[3]), (0, 0, 255), 1)
                 msk_idx = np.where(mask == 1)
-                # pmsk_idx = np.where(predmask == 1)
+                pmsk_idx = np.where(predmask == 1)
                 img_copy[msk_idx[0], msk_idx[1], 0] = 255
-                # img_copy[pmsk_idx[0], pmsk_idx[1], 0] = 255
+                img_copy[pmsk_idx[0], pmsk_idx[1], 2] = 255
             # cv2.imshow(str(batch_id) + " " + str(i), img_copy)
-            cv2.imwrite('simple_test\\test_sample.jpg', img_copy*255)
+            cv2.imwrite('simple_test\\test_sample_overlap.jpg', img_copy*255)
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
 

toyexample/toylearner.py
@@ -3,6 +3,7 @@ import time
 import gc
 import numpy as np
 import torch
+from torchvision.models.detection.rpn import AnchorGenerator, RPNHead
 from torchvision.transforms import functional as TF
 from torch.optim import lr_scheduler, SGD
 from torchvision.models.detection import maskrcnn_resnet50_fpn
@@ -120,7 +121,12 @@ targets_crop = [elem_crop]
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
-pretrained = True
+anchor_generator = AnchorGenerator(
+    sizes=tuple([(16, 24, 32, 48, 96) for _ in range(5)]),
+    aspect_ratios=tuple([(0.5, 1.0, 2.0) for _ in range(5)]))
+rpnhead = RPNHead(256, anchor_generator.num_anchors_per_location()[0])
+
+pretrained = False
 
 if pretrained:
     model = maskrcnn_resnet50_fpn(pretrained=True, min_size=512, max_size=512)
@@ -148,15 +154,20 @@ if pretrained:
     optimizer_ft = SGD(params, lr=0.001, momentum=0.9, weight_decay=0.0001)
 
 else:
-    # Observe that all parameters are being optimized
-    model = maskrcnn_resnet50_fpn(num_classes=2)
+    # Observe that all parameters are being optimized (except for the parameters of the pretrained backbone)
+    model = maskrcnn_resnet50_fpn(num_classes=2, max_size=512, rpn_head=rpnhead
+                                  , rpn_anchor_generator=anchor_generator, rpn_pre_nms_top_n_train=12000
+                                  , rpn_pre_nms_top_n_test=6000, rpn_post_nms_top_n_train=2000
+                                  , rpn_post_nms_top_n_test=300, rpn_fg_iou_thresh=0.5, rpn_bg_iou_thresh=0.3
+                                  , rpn_positive_fraction=0.7, bbox_reg_weights=(1.0, 1.0, 1.0, 1.0)
+                                  , box_batch_size_per_image=32)
     optimizer_ft = SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=0.0001)
 
 # don't know how to initialize the weights of the model
 # torch.nn.init.kaiming_normal_(model.parameters(), mode='fan_out')
 
 # Decay LR by a factor of 0.1 every 2 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=2, gamma=0.1)
+# exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=2, gamma=0.1)
 
 num_epochs = 100
 since = time.time()
@@ -165,8 +176,8 @@ model.train()
 print('Pretrained:' + str(pretrained))
 print('momentum:' + str(optimizer_ft.state_dict()['param_groups'][0]['momentum']))
 print('weight_decay:' + str(optimizer_ft.state_dict()['param_groups'][0]['weight_decay']))
-print('LR decay gamma:' + str(exp_lr_scheduler.state_dict()['gamma']))
-print('LR decay step size:' + str(exp_lr_scheduler.state_dict()['step_size']))
+# print('LR decay gamma:' + str(exp_lr_scheduler.state_dict()['gamma']))
+# print('LR decay step size:' + str(exp_lr_scheduler.state_dict()['step_size']))
 
 for epoch in range(num_epochs):
     print('\nEpoch {}/{}'.format(epoch, num_epochs - 1))
@@ -185,7 +196,7 @@ for epoch in range(num_epochs):
     # perform backward propagation, optimization and update model parameters
     losses.backward()
     optimizer_ft.step()
-    exp_lr_scheduler.step()
+    # exp_lr_scheduler.step()
     del losses
     gc.collect()
 